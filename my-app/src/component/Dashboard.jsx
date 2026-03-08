93465bd62a0290ab67b28978d6a78e443f38acf8
samuel Dejesus <samuel1de2jesus3@gmail.com>
2026-03-07 20:39:34 -0500

    modify the api call so i can get weather data

--- my-app/src/component/Dashboard.jsx
+++ my-app/src/component/Dashboard.jsx
@@ -5,13 +5,20 @@ function Dashboard() {
   const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
 
   useEffect(() => {
-    fetch(`http://api.openweathermap.org/geo/1.0/direct?q={Orlando},{FL},{USA}&appid=${apiKey}`)
+    fetch(`https://api.openweathermap.org/geo/1.0/direct?q=Orlando,FL,US&limit=1&appid=${apiKey}`)
       .then(res => res.json())
-      .then(data => console.log(data));
+      .then(data => {
+        const lat = data[0].lat;
+        const lon = data[0].lon;
+
+        return fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`);
+      })
+      .then(res => res.json())
+      .then(weather => console.log(weather));
   }, []);
 
   return(
-    <div className="col-12 d-flex column justify-content-center mt-5 border">
+    <div className="col-12 d-flex flex-column justify-content-center mt-5 border">
       <h1>Current weather in Orlando, FL</h1>
     </div>
   );
