0f4793710712be317608cef77a70510498384f42
samuel Dejesus <samuel1de2jesus3@gmail.com>
2026-03-07 20:11:11 -0500

    fixed code api key now works

--- my-app/src/component/Dashboard.jsx
+++ my-app/src/component/Dashboard.jsx
@@ -1,16 +1,20 @@
+import { useEffect } from "react";
 
-function Dashboard(){
+function Dashboard() {
 
+  const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
 
-    return(
-        <div className="col-12 d-flex column justify-content-center mt-5 border">
-            <h1>Current weather in Orlando, FL</h1>
+  useEffect(() => {
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q={Orlando},{FL},{USA}&appid=${apiKey}`)
+      .then(res => res.json())
+      .then(data => console.log(data));
+  }, []);
 
-
-        </div>
-
-    )
-
+  return(
+    <div className="col-12 d-flex column justify-content-center mt-5 border">
+      <h1>Current weather in Orlando, FL</h1>
+    </div>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
